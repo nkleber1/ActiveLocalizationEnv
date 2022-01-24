d82f1eb790d1b3154142e09776b0d05c850a2615
nkleber <nils.kleber@freenet.de>
2022-01-24 12:13:34 +0100

    add vtk classes

---
+++ activeloc/vtk/__init__.py
@@ -0,0 +1,6 @@
+from .bayes_filter import CustomBayesFilter
+from .lasers import Lasers
+from .measurement import LaserMeasurements
+from .mesh import Mesh
+from .belief import PositionBelief
+from .renderer import Renderer
