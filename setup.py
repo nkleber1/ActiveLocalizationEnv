afc03d7b507621b8d89975e2be7d193b1ee4f143
nkleber <nils.kleber@freenet.de>
2022-01-25 12:13:42 +0100

    add simple setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+# TODO Add requirements.txt
+setup(name='active_localization_env',
+      version='0.1.0',
+      description='A gym-style environment in which reinforcement learning agents can learn active localisation',
+      author='Nils Kleber',
+      author_email='nkleber@student.ethz.ch',
+      url='https://github.com/nkleber1',
+      py_modules=['active_localization_env'],
+     )
