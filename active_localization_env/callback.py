85bc033539b4f44ffdf9d766e6b224643d4c7f9b
nkleber <nils.kleber@freenet.de>
2022-01-25 14:15:49 +0100

    add TensorboardCallback

--- active_localization_env/callback.py
+++ active_localization_env/callback.py
@@ -9,6 +9,24 @@ class TensorboardCallback(BaseCallback):
     """
     def __init__(self, steps_per_summary, steps_per_update, verbose=0):
         super(TensorboardCallback, self).__init__(verbose)
+        # Those variables will be accessible in the callback
+        # (they are defined in the base class)
+        # The RL model
+        # self.model = None  # type: BaseAlgorithm
+        # An alias for self.model.get_env(), the environment used for training
+        # self.training_env = None  # type: Union[gym.Env, VecEnv, None]
+        # Number of time the callback was called
+        # self.n_calls = 0  # type: int
+        # self.num_timesteps = 0  # type: int
+        # local and global variables
+        # self.locals = None  # type: Dict[str, Any]
+        # self.globals = None  # type: Dict[str, Any]
+        # The logger object, used to report things in the terminal
+        # self.logger = None  # stable_baselines3.common.logger
+        # # Sometimes, for event callback, it is useful
+        # # to have access to the parent object
+        # self.parent = None  # type: Optional[BaseCallback]
+
         self.rollouts_per_summary = steps_per_summary/steps_per_update
         self.rollout_cnt = 0
         self.vol_mean = 0
